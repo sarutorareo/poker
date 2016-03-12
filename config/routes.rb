c775b56901eade626a75691ea77667c0a70c154b
sarutorareo <sarutorareo@gmail.com>
2016-03-12 20:02:27 +0900

    ユーザのindexを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  root 'top#index'
+  root "top#index"
   get 'top/index'
+
+  resources "users"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
--- db/seeds/development/users.rb
+++ db/seeds/development/users.rb
@@ -2,8 +2,8 @@ names = %w(Jiro Saburo Hana Hiyoten)
 
 0.upto(3) do |idx|
   User.create(
-    name: names{idx},
-    chip: 1000
+    name: names[idx],
+    chip: 1000,
     administrator: (idx == 0)
   )
 end
