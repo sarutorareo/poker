c96b360d39a849bd76a9c44dbc7d3e485f8964ba
sarutorareo <sarutorareo@gmail.com>
2016-03-23 17:10:18 +0900

    チャットページのレイアウトを作成

--- app/controllers/chat_controller.rb
+++ app/controllers/chat_controller.rb
@@ -2,6 +2,7 @@ class ChatController < ApplicationController
   def index
     room = Room.find(params[:room_id]) if params[:room_id]
     @room_name = "room" + "%02d" % room.no if room
+    @tern_chat = (@room_name == "room01")
     @user_name = params[:user_name]
   end
 end
